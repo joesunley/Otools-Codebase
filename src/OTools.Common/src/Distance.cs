ab9c770a4c07d77004d89a465167c1b030d6201d
Joe Sunley <Joe@jossy.co.uk>
2023-08-10 13:00:59 +0100

    Created a Distance type

---
+++ src/OTools.Common/src/Distance.cs
@@ -0,0 +1,44 @@
+﻿namespace OTools.Common;
+
+public struct Distance : IEquatable<Distance>, IComparable<Distance>
+{
+    private long _value; // Distance in micrometres
+
+    private Distance(long value) => _value = value;
+
+
+    public decimal Millimetres => _value / 1_000m;
+    public decimal Centimetres => _value / 10_000m;
+    public decimal Metres => _value / 1_000_000m;
+    public decimal Kilometres => _value / 1_000_000_000m;
+
+
+    public static Distance FromMillimetres(decimal value)
+    {
+        return new((long)(value * 1_000));
+    }
+
+    public static Distance FromCentimetres(decimal value)
+    {
+        return new((long)(value * 10_000));
+    }
+
+    public static Distance FromMetres(decimal value)
+    {
+        return new((long)(value * 1_000_000));
+    }
+
+    public static Distance FromKilometres(decimal value)
+    {
+        return new((long)(value * 1_000_000_000));
+    }
+
+    public bool Equals(Distance other)
+    {
+        return _value == other._value;
+    }
+    public int CompareTo(Distance other)
+    {
+        return _value.CompareTo(other._value);
+    }
+}
