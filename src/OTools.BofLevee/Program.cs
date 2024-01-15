211ad3e7f4fdbb85964256109b468448736b5c4b
Joe Sunley <joseph@jossy.co.uk>
2024-01-15 15:09:05 +0000

    Update Bof Levee calculator to use results

--- src/OTools.BofLevee/Program.cs
+++ src/OTools.BofLevee/Program.cs
@@ -4,15 +4,8 @@ string filePath;
 
 if (args.Length == 0)
 {
-    Console.WriteLine(Directory.GetFiles(Environment.CurrentDirectory));
-
-    if (Directory.GetFiles(Environment.CurrentDirectory).Contains("Standard Export.csv"))
-        filePath = Path.Combine(Environment.CurrentDirectory, "Standard Export.csv");
-    else
-    {
-        Console.Write("Filepath: ");
-        filePath = Console.ReadLine() ?? string.Empty;
-    }
+    Console.Write("Filepath: ");
+    filePath = Console.ReadLine() ?? string.Empty;
 }
 else filePath = args[0];
 
@@ -22,30 +15,67 @@ string[] lines = File.ReadAllLines(filePath);
 (int s, int j) members = (0, 0);
 (int s, int j) nonMembers = (0, 0);
 
+List<string> names = new();
+
+//foreach (string line in lines)
+//{
+//    if (line == lines[0]) continue;
+
+//    string[] values = line.Split(',');
+
+//    DateOnly dob = DateOnly.Parse(values[6]);
+//    string bofNum = values[1];
+
+//    int age = DateTime.Now.Year - dob.Year;
+
+//    if (age <= 20)
+//    {
+//        if (!string.IsNullOrEmpty(bofNum))
+//            members.j++;
+//        else
+//            nonMembers.j++;
+//    }
+//    else
+//    {
+//        if (!string.IsNullOrEmpty(bofNum))
+//            members.s++;
+//        else
+//            nonMembers.s++;
+//    }
+//}
+
 foreach (string line in lines)
 {
     if (line == lines[0]) continue;
 
     string[] values = line.Split(',');
 
-    DateOnly dob = DateOnly.Parse(values[6]);
-    string bofNum = values[1];
+    string member = values[2];
+    string agecat = values[4];
 
-    int age = DateTime.Now.Year - dob.Year;
+    bool isSen = false;
 
-    if (age <= 20)
+    if (agecat.Length <= 1)
+        isSen = true;
+
+    if (!isSen && int.Parse(agecat[1..].ToString() ?? "99") >= 21)
+        isSen = true;
+
+    if (member != "")
     {
-        if (!string.IsNullOrEmpty(bofNum))
-            members.j++;
+        if (isSen)
+            members.s++;
         else
-            nonMembers.j++;
+            members.j++;
     }
     else
     {
-        if (!string.IsNullOrEmpty(bofNum))
-            members.s++;
-        else
+        names.Add(values[3]);
+
+        if (isSen)
             nonMembers.s++;
+        else
+            nonMembers.j++;
     }
 }
 
@@ -57,5 +87,9 @@ table.AddRow("Member", members.s.ToString(), members.j.ToString());
 table.AddRow("Non-Member", nonMembers.s.ToString(), nonMembers.j.ToString());
 
 AnsiConsole.Write(table);
+Console.WriteLine();
+
+foreach (string n in names)
+    Console.WriteLine(n);
 
 Console.ReadLine();
